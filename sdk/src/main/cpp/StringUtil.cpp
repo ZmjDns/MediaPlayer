c4917469289c3abcf9e4f4db5e116f2f87f3296a
ZmjDns <292570927@qq.com>
2020-02-23 22:57:13 +0800

    model中实现c代码

--- sdk/src/main/cpp/StringUtil.cpp
+++ sdk/src/main/cpp/StringUtil.cpp
@@ -9,7 +9,7 @@ JNIEXPORT jstring
 JNICALL
 Java_com_zmj_sdk_util_StringUtil_getStringNDK(JNIEnv *env,jobject instance)
 {
-    string result = "Hello from NDK";
+    string result = "Hello from NDK aa";
     return env->NewStringUTF(result.c_str());
 }
 
